c9886207d532977f4aad515d680bc5a8a22f0b95
Arthur McLain <mclain.it@gmail.com>
2022-03-11 23:20:26 +0100

    Rewrite most of remaps to lua

---
+++ nvim/.config/nvim/lua/trimclain/remaps.lua
@@ -0,0 +1,105 @@
+-- ############################################################################
+-- Remaps
+-- ############################################################################
+
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+local keymap = vim.api.nvim_set_keymap
+
+-- Remap space as a leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- TODO: how to write this in lua (~ isn't recognized)
+vim.cmd "nnoremap <silent> <Leader><CR> :so ~/.config/nvim/init.vim<CR>"
+
+-- Easier movement between vim windows
+keymap("n", "<leader>h", "<C-w>h", opts)
+keymap("n", "<leader>j", "<C-w>j", opts)
+keymap("n", "<leader>k", "<C-w>k", opts)
+keymap("n", "<leader>l", "<C-w>l", opts)
+
+-- Navigate buffers
+keymap("n", "<S-l>", ":bnext<cr>", opts)
+keymap("n", "<S-h>", ":bprevious<cr>", opts)
+
+-- Resizing: Use Ctrl + arrows to resize windows
+keymap("n", "<C-Up>", ":resize -5<cr>", opts)
+keymap("n", "<C-Down>", ":resize +5<cr>", opts)
+keymap("n", "<C-Left>", ":vertical resize -5<cr>", opts)
+keymap("n", "<C-Right>", ":vertical resize +5<cr>", opts)
+
+-- Vim-fugitive
+keymap("n", "<leader>gs", ":G<cr>", opts)   -- git status
+-- resolve conflicts when mergin branches (not used)
+-- keymap("n", "<leader>gj", ":diffget //3<cr>", opts)
+-- keymap("n", "<leader>gf", ":diffget //2<cr>", opts)
+
+-- Project View (File Explorer)
+-- keymap("n", "<leader>pv", ":Ex<cr>", opts)
+keymap("n", "<leader>pv", ":Lex 30<cr>", opts)
+
+-- Undotree
+keymap("n", "<leader>u", ":UndotreeShow<cr>", opts)
+
+-- Terminal
+keymap("n", "<leader>tt", "<C-w>v<C-w>l :terminal<cr>", term_opts)  --in the right vsplit
+keymap("t", "<Esc>", "<C-\\><C-n>", term_opts)                      -- leave terminal mode with <Esc>
+
+-- QuickFixList Stuff - local ones not in use
+QFLIST_LOCAL = 0
+QFLIST_GLOBAL = 0
+
+-- function ToggleQFList()
+--
+-- end
+-- Very Useful Stuff
+keymap("n", "Q", "", opts)                  -- disable Q coz useless
+keymap("n", "<cr>", ":noh<cr>", opts)       -- disable the higlighting of the searched text
+keymap("n", "Y", "y$", opts)                -- make Y work like C and D
+keymap("n", "n", "nzzzv", opts)             -- keep it centered when searching forward
+keymap("n", "N", "Nzzzv", opts)             -- and backwards
+keymap("n", "J", "mzJ`z", opts)             -- keep it centered when joining lines
+keymap("i", ",", ",<c-g>u", opts)           -- set a break point for undo after ,
+keymap("i", ".", ".<c-g>u", opts)           -- after .
+keymap("i", "!", "!<c-g>u", opts)           -- after !
+keymap("i", "?", "?<c-g>u", opts)           -- after ?
+keymap("v", "K", ":m '<-2<cr>gv=gv", opts)  -- move higlighted lines up a line
+keymap("v", "J", ":m '>+1<cr>gv=gv", opts)  -- and down a line
+keymap("v", "<", "<gv", opts)               -- stay in indent mode
+keymap("v", ">", ">gv", opts)               -- stay in indent mode
+keymap("v", "y", "myy`y", opts)             -- Maintain the cursor position
+keymap("v", "Y", "myy`y", opts)             -- when yanking a visual selection
+keymap("v", "p", '"_dP', opts)              -- when replacing a higlighted text, don't yank it
+keymap("n", "<leader>Y", 'gg"+yG', opts)    -- yank whole file
+keymap("n", "<leader>y", '"+y', opts)       -- yank not hurting
+keymap("v", "<leader>y", '"+y', opts)       -- the registers
+keymap("n", "<leader>d", '"_d', opts)       -- delete into
+keymap("v", "<leader>d", '"_d', opts)       -- blackhole buffer
+
+-- Jumplist mutations: when you do 15j in normal mode and than C-o, it wouldn't
+-- do 15k for you. With next remaps you modify your Jumplist (used by C-o and C-i)
+-- to add break points every time you jump more than 5 lines up or down.
+-- Maybe I'll rewrite this in lua someday
+vim.cmd [[nnoremap <expr> k (v:count > 5 ? "m'" . v:count : "") . 'k']]
+vim.cmd [[nnoremap <expr> j (v:count > 5 ? "m'" . v:count : "") . 'j']]
+
+-- Esc is too far and I don't like <C-[>, make <C-c> work as <Esc> in every mode
+keymap("", "<C-c>", "<Esc>", opts)      -- normal, visual, select, operator-pending modes
+keymap("l", "<C-c>", "<Esc>", opts)     -- insert, command-line, lang-arg modes
+keymap("i", "<C-c>", "<Esc>", opts)     -- insert mode again, coz doesn't work above
+
+-- Check the filetype to know how to run the file
+if vim.bo.filetype == "python" then
+    -- if a python file
+    keymap("n", "<C-b>", ":w <bar> :! python3 %<cr>", opts)
+elseif vim.bo.filetype == "sh" then
+    -- bash, shell, zsh -> all will run in bash coz why not
+    keymap("n", "<C-b>", ":w <bar> :! bash %<cr>", opts)
+else
+    -- other executable files
+    keymap("n", "<C-b>", ":w <bar> :! ./%<cr>", opts)
+end
