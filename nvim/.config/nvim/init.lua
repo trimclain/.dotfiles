a2ed69f0d81522f8577e05c661583e90b8d156ba
Arthur McLain <mclain.it@gmail.com>
2022-03-12 14:10:28 +0100

    Move to lua part 6 (almost there)

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,26 @@
+--#############################################################################
+--        ______ ___    __ __   ___    ____  __     __     __  __   __
+--       /_  __//   \  / //  \ /   |  / __/ / /    /  |   / / /  \ / /
+--        / /  /   _/ / // /\\/ /| | / /   / /    /   |  / / / /\\/ /
+--       / /  / /\ \ / // / \/_/ | |/ /__ / /___ / _  | / / / / \/ /
+--      /_/  /_/ \_\/_//_/       |_|\___//_____//_/ \_|/_/ /_/  /_/
+--
+--
+--       Arthur McLain (trimclain)
+--       mclain.it@gmail.com
+--       https://github.com/trimclain
+--
+--#############################################################################
+
+require("trimclain.settings")
+require("trimclain.remaps")
+require("trimclain.plugins")
+require("trimclain.catppuccin")
+require("trimclain.colorscheme")
+require("trimclain.lualine")
+require("trimclain.autopairs")
+require("trimclain.telescope")
+-- require("trimclain.lsp")
+-- require("trimclain.luasnip")
+-- require("trimclain.treesitter")
+require("trimclain.autocmd")
