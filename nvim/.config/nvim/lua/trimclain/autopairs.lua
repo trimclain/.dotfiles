469b7afe2dd6fa1c12a25874b77396f7a07e9070
Arthur McLain <mclain.it@gmail.com>
2022-03-13 20:13:10 +0100

    Modify nvim-autopairs config

--- nvim/.config/nvim/lua/trimclain/autopairs.lua
+++ nvim/.config/nvim/lua/trimclain/autopairs.lua
@@ -1,12 +1,45 @@
-require('nvim-autopairs').setup({
+local status_ok, npairs = pcall(require, "nvim-autopairs")
+if not status_ok then
+  return
+end
+
+npairs.setup({
+    check_ts = true,        -- work with treesitter
+    ts_config = {
+        lua = { "string", "source" },
+        javascript = { "string", "template_string" },
+        java = false,
+    },
     disable_filetype = { "TelescopePrompt" , "vim" },
     disable_in_macro = true,  -- disable when recording or executing a macro
+    disable_in_visualblock = false, -- disable when insert after visual block mode
     ignored_next_char = string.gsub([[ [%w%%%'%[%"%.] ]],"%s+", ""),
     enable_moveright = true,
     enable_afterquote = true,  -- add bracket pairs after quote
     enable_check_bracket_line = true,  --- check bracket in same line
-    check_ts = false,
     map_bs = true,  -- map the <BS> key
-    map_c_w = false -- map <c-w> to delete a pair if possible
+    map_c_h = false, -- Map the <C-h> key to delete a pair
+    map_c_w = false, -- map <C-w> to delete a pair if possible
+    fast_wrap = {
+        map = "<M-c>",
+        chars = { "{", "[", "(", '"', "'" },
+        pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+        offset = 0, -- Offset from pattern match
+        end_key = "$",
+        keys = "qwertyuiopzxcvbnmasdfghjkl",
+        check_comma = true,
+        -- highlight = "PmenuSel",
+        -- highlight_grey = "LineNr",
+        highlight = 'Search',
+        highlight_grey='Comment'
+    },
 })
 
+-- You need to add mapping `CR` on nvim-cmp setup. Check readme.md on nvim-cmp repo.
+-- If you want insert `(` after select function or method item
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+local cmp_status_ok, cmp = pcall(require, "cmp")
+if not cmp_status_ok then
+  return
+end
+cmp.event:on( "confirm_done", cmp_autopairs.on_confirm_done({  map_char = { tex = "" } }))
