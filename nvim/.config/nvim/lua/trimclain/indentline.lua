6ec30f8c8a1b6e58031ce92359ea24529ddd128b
Arthur McLain <mclain.it@gmail.com>
2022-03-18 01:59:43 +0100

    Update indentline.lua

--- nvim/.config/nvim/lua/trimclain/indentline.lua
+++ nvim/.config/nvim/lua/trimclain/indentline.lua
@@ -10,6 +10,7 @@ vim.g.indent_blankline_filetype_exclude = {
 	"dashboard",
 	"packer",
 	"neogitstatus",
+	"undotree",
 	"NvimTree",
 	"Trouble",
 }
@@ -44,7 +45,7 @@ vim.g.indent_blankline_context_patterns = {
 	"operation_type",
 }
 -- HACK: work-around for https://github.com/lukas-reineke/indent-blankline.nvim/issues/59
-vim.wo.colorcolumn = "99999"
+-- vim.wo.colorcolumn = "99999"
 
 -- vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
 -- vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
@@ -52,10 +53,18 @@ vim.wo.colorcolumn = "99999"
 -- vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
 -- vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
 -- vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
+
+-- Next lines add the ↲ sign at the EOL
 vim.opt.list = true
 -- vim.opt.listchars:append "space:⋅"
 -- vim.opt.listchars:append "space:"
-vim.opt.listchars:append ("eol:↴")
+-- vim.opt.listchars:append ("eol:↴")
+-- vim.opt.listchars:append ("eol:﬋")
+-- vim.opt.listchars:append ("eol:⤶")
+vim.opt.listchars:append ("eol:↲")
+
+-- To set or unset the char for a trailing space (default is "trail:-")
+vim.opt.listchars:append ("trail: ") -- currently it's unset
 
 indent_blankline.setup({
 	show_end_of_line = true,
