daa344eb06c457e795b145bc1b468c2201977a65
Arthur McLain <mclain.it@gmail.com>
2022-04-08 18:41:36 +0200

    Update prettier config

--- nvim/.config/nvim/lua/trimclain/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/trimclain/lsp/null-ls.lua
@@ -11,7 +11,7 @@ local diagnostics = null_ls.builtins.diagnostics
 null_ls.setup {
     debug = false,
     sources = {
-        formatting.prettier.with { extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } },
+        formatting.prettier.with { extra_args = { "--tab-width=4" } }, -- , "--no-semi", "--single-quote", "--jsx-single-quote"
         formatting.black.with {
             extra_args = {
                 "--fast", -- if --fast given, skip temporary sanity checks [default: --safe]
