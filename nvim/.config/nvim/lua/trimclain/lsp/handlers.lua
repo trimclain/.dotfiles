9dcd73f492c9d8299b655af7ad17882df1c4d835
Arthur McLain <mclain.it@gmail.com>
2022-06-04 01:57:01 +0200

    Remove unused code

--- nvim/.config/nvim/lua/trimclain/lsp/handlers.lua
+++ nvim/.config/nvim/lua/trimclain/lsp/handlers.lua
@@ -43,23 +43,6 @@ M.setup = function()
     })
 end
 
--- don't want the document_highlight yet
--- local function lsp_highlight_document(client)
--- 	-- Set autocommands conditional on server_capabilities
--- 	if client.resolved_capabilities.document_highlight then
--- 		vim.api.nvim_exec(
--- 			[[
--- 			augroup lsp_document_highlight
--- 				autocmd! * <buffer>
--- 				autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
--- 				autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
--- 			augroup END
--- 		]],
--- 			false
--- 		)
--- 	end
--- end
-
 local function lsp_keymaps(bufnr)
     local opts = { noremap = true, silent = true }
     vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
@@ -70,7 +53,6 @@ local function lsp_keymaps(bufnr)
     vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
     vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
     -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-    -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>f", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
     vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
     vim.api.nvim_buf_set_keymap(
         bufnr,
@@ -92,7 +74,6 @@ M.on_attach = function(client, bufnr)
         client.server_capabilities.documentFormattingProvider = false -- 0.8 and hopefully later
     end
     lsp_keymaps(bufnr)
-    -- lsp_highlight_document(client)
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
