3b5cb8e8074a1e66a34347535079a0e81c914f7b
Arthur McLain <mclain.it@gmail.com>
2022-06-07 16:59:32 +0200

    Update nvim-tree config

--- nvim/.config/nvim/lua/trimclain/nvim-tree.lua
+++ nvim/.config/nvim/lua/trimclain/nvim-tree.lua
@@ -37,57 +37,31 @@ local tree_cb = nvim_tree_config.nvim_tree_callback
 -- following options are the default
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
 nvim_tree.setup {
-    disable_netrw = true,
-    hijack_netrw = true,
-    open_on_setup = false,
-    ignore_buffer_on_setup = false,
-    ignore_ft_on_setup = {
-        "startify",
-        "dashboard",
-        "alpha",
-    },
     auto_reload_on_write = true,
-    open_on_tab = false,
-    hijack_cursor = true,
-    update_cwd = true,
+    create_in_closed_folder = false,
+    disable_netrw = true, -- default: false
+    hijack_cursor = true, -- default: false
+    hijack_netrw = true,
     hijack_unnamed_buffer_when_opening = false,
-    hijack_directories = {
-        enable = true,
-        auto_open = true,
-    },
-    diagnostics = {
-        enable = true,
-        icons = {
-            hint = "",
-            info = "",
-            warning = "",
-            error = "",
-        },
-    },
-    update_focused_file = {
-        enable = true,
-        update_cwd = true,
-        ignore_list = {},
-    },
-    system_open = {
-        cmd = nil,
-        args = {},
-    },
-    filters = {
-        dotfiles = false,
-        custom = {},
-    },
-    git = {
-        enable = true,
-        ignore = true,
-        timeout = 500,
-    },
+    ignore_buffer_on_setup = false,
+    open_on_setup = false,
+    open_on_setup_file = false,
+    open_on_tab = false,
+    sort_by = "name",
+    update_cwd = true, -- default: false
+    reload_on_bufenter = false,
+    respect_buf_cwd = false,
     view = {
+        adaptive_size = false,
+        centralize_selection = false,
         width = 30,
         height = 30,
         hide_root_folder = false,
         side = "left",
         preserve_window_proportions = false,
+        number = false,
+        relativenumber = false,
+        signcolumn = "yes",
         mappings = {
             custom_only = false,
             list = {
@@ -96,22 +70,115 @@ nvim_tree.setup {
                 { key = "v", cb = tree_cb "vsplit" },
             },
         },
-        number = false,
-        relativenumber = false,
-        signcolumn = "yes",
     },
-    trash = {
-        cmd = "trash",
-        require_confirm = true,
+    renderer = {
+        add_trailing = false,
+        group_empty = false,
+        highlight_git = false,
+        full_name = false,
+        highlight_opened_files = "none",
+        root_folder_modifier = ":~",
+        indent_markers = {
+            enable = false,
+            icons = {
+                corner = "└ ",
+                edge = "│ ",
+                item = "│ ",
+                none = "  ",
+            },
+        },
+        icons = {
+            webdev_colors = true,
+            git_placement = "before",
+            padding = " ",
+            symlink_arrow = " ➛ ",
+            show = {
+                file = true,
+                folder = true,
+                folder_arrow = true,
+                git = true,
+            },
+            glyphs = {
+                default = "",
+                symlink = "",
+                folder = {
+                    arrow_closed = "",
+                    arrow_open = "",
+                    default = "",
+                    open = "",
+                    empty = "",
+                    empty_open = "",
+                    symlink = "",
+                    symlink_open = "",
+                },
+                git = {
+                    unstaged = "✗",
+                    staged = "✓",
+                    unmerged = "",
+                    renamed = "➜",
+                    untracked = "★",
+                    deleted = "",
+                    ignored = "◌",
+                },
+            },
+        },
+        special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md" },
+    },
+    hijack_directories = {
+        enable = true,
+        auto_open = true,
+    },
+    update_focused_file = {
+        enable = true, -- default: false
+        update_cwd = true, -- default: false
+        ignore_list = {},
+    },
+    ignore_ft_on_setup = {
+        "startify",
+        "dashboard",
+        "alpha",
+    },
+    system_open = {
+        cmd = "",
+        args = {},
+    },
+    diagnostics = {
+        enable = false,
+        show_on_dirs = false,
+        icons = {
+            hint = "",
+            info = "",
+            warning = "",
+            error = "",
+        },
+    },
+    filters = {
+        dotfiles = false,
+        custom = {},
+        exclude = {},
+    },
+    filesystem_watchers = {
+        enable = false,
+        interval = 100,
+    },
+    git = {
+        enable = true,
+        ignore = true,
+        timeout = 400,
     },
     actions = {
+        use_system_clipboard = true,
         change_dir = {
             enable = true,
             global = false,
+            restrict_above_cwd = false,
+        },
+        expand_all = {
+            max_folder_discovery = 300,
         },
         open_file = {
-            quit_on_open = false,
-            resize_window = false,
+            quit_on_open = true, -- default: false
+            resize_window = true,
             window_picker = {
                 enable = true,
                 chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
@@ -121,6 +188,17 @@ nvim_tree.setup {
                 },
             },
         },
+        remove_file = {
+            close_window = true,
+        },
+    },
+    trash = {
+        cmd = "gio trash",
+        require_confirm = true,
+    },
+    live_filter = {
+        prefix = "[FILTER]: ",
+        always_show_folders = true,
     },
     log = {
         enable = false,
@@ -128,7 +206,11 @@ nvim_tree.setup {
         types = {
             all = false,
             config = false,
+            copy_paste = false,
+            diagnostics = false,
             git = false,
+            profile = false,
+            watcher = false,
         },
     },
 }
