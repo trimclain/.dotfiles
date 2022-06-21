bd9a2e0e1d0f900405cb5aebc87fbde9b09957e8
Arthur McLain <mclain.it@gmail.com>
2022-06-21 20:08:39 +0200

    Add julia support

--- nvim/.config/nvim/lua/trimclain/lsp/configs.lua
+++ nvim/.config/nvim/lua/trimclain/lsp/configs.lua
@@ -1,6 +1,6 @@
 local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
 if not status_ok then
-	return
+    return
 end
 
 local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
@@ -18,10 +18,11 @@ local servers = {
     "tsserver",
     "vimls",
     "yamlls",
+    "julials",
 }
 
 lsp_installer.setup {
-	ensure_installed = servers,
+    ensure_installed = servers,
     -- automatic_installation = false,
     ui = {
         icons = {
@@ -49,13 +50,13 @@ lsp_installer.setup {
 }
 
 for _, server in pairs(servers) do
-	local opts = {
-		on_attach = require("trimclain.lsp.handlers").on_attach,
-		capabilities = require("trimclain.lsp.handlers").capabilities,
-	}
-	local has_custom_opts, server_custom_opts = pcall(require, "trimclain.lsp.settings." .. server)
-	if has_custom_opts then
-	    opts = vim.tbl_deep_extend("force", server_custom_opts, opts)
-	end
-	lspconfig[server].setup(opts)
+    local opts = {
+        on_attach = require("trimclain.lsp.handlers").on_attach,
+        capabilities = require("trimclain.lsp.handlers").capabilities,
+    }
+    local has_custom_opts, server_custom_opts = pcall(require, "trimclain.lsp.settings." .. server)
+    if has_custom_opts then
+        opts = vim.tbl_deep_extend("force", server_custom_opts, opts)
+    end
+    lspconfig[server].setup(opts)
 end
