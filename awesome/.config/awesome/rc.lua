573bc7069fc09c72acee965717b51d0a8bf301ab
Arthur McLain <mclain.it@gmail.com>
2022-06-26 15:14:08 +0200

    Disable scrolling through tags

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -337,9 +337,10 @@ end)
 root.buttons(mytable.join(
     awful.button({}, 3, function()
         awful.util.mymainmenu:toggle()
-    end),
-    awful.button({}, 4, awful.tag.viewnext),
-    awful.button({}, 5, awful.tag.viewprev)
+    end)
+    -- I don't want the Scrolling Wheel to move me to next tag
+    -- awful.button({}, 4, awful.tag.viewnext),
+    -- awful.button({}, 5, awful.tag.viewprev)
 ))
 -- }}}
 
