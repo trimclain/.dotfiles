979b4e6a9003496eb6e5e48de8db9347c98ff29c
Arthur McLain <mclain.it@gmail.com>
2022-06-28 23:41:38 +0200

    Add theme updater module to polybar

---
+++ polybar/.config/polybar/scripts/color/colors-light.sh
@@ -0,0 +1,121 @@
+#!/usr/bin/env bash
+
+# Color files
+PFILE="$HOME/.config/polybar/colors.ini"
+# RFILE="$HOME/.config/polybar/docky/scripts/rofi/colors.rasi"
+
+# Change colors
+change_color() {
+    # polybar
+    sed -i -e 's/background = #.*/background = #dfdfe0/g' $PFILE
+    sed -i -e 's/background-alt = #.*/background-alt = #e7e8eb/g' $PFILE
+    sed -i -e 's/foreground = #.*/foreground = #292a30/g' $PFILE
+    sed -i -e 's/foreground-alt = #.*/foreground-alt = #414453/g' $PFILE
+    sed -i -e "s/primary = #.*/primary = $PC/g" $PFILE
+    sed -i -e "s/secondary = #.*/secondary = $SC/g" $PFILE
+    sed -i -e 's/alert = #.*/alert = #ff5555/g' $PFILE
+    sed -i -e 's/disabled = #.*/disabled = #7f8c98/g' $PFILE
+
+    # # rofi
+    # cat > $RFILE <<- EOF
+    # /* colors */
+    #
+    # * {
+    #     al:   #00000000;
+    #     bg:   #FFFFFFFF;
+    #     bga:  ${PC}33;
+    #     bar:  ${SC}FF;
+    #     fg:   #2E2E2EFF;
+    #     ac:   ${PC}FF;
+    # }
+    # EOF
+
+    polybar-msg cmd restart
+}
+
+if  [[ $1 = "--amber" ]]; then
+    SC="#2E2E2E"
+    PC="#ffb300"
+    change_color
+elif  [[ $1 = "--blue" ]]; then
+    SC="#FFFFFF"
+    PC="#1e88e5"
+    change_color
+elif  [[ $1 = "--blue-gray" ]]; then
+    SC="#FFFFFF"
+    PC="#546e7a"
+    change_color
+elif  [[ $1 = "--brown" ]]; then
+    SC="#FFFFFF"
+    PC="#6d4c41"
+    change_color
+elif  [[ $1 = "--cyan" ]]; then
+    SC="#2E2E2E"
+    PC="#00acc1"
+    change_color
+elif  [[ $1 = "--deep-orange" ]]; then
+    SC="#FFFFFF"
+    PC="#f4511e"
+    change_color
+elif  [[ $1 = "--deep-purple" ]]; then
+    SC="#FFFFFF"
+    PC="#5e35b1"
+    change_color
+elif  [[ $1 = "--green" ]]; then
+    SC="#FFFFFF"
+    PC="#43a047"
+    change_color
+elif  [[ $1 = "--gray" ]]; then
+    SC="#FFFFFF"
+    PC="#757575"
+    change_color
+elif  [[ $1 = "--indigo" ]]; then
+    SC="#FFFFFF"
+    PC="#3949ab"
+    change_color
+elif  [[ $1 = "--light-blue" ]]; then
+    SC="#2E2E2E"
+    PC="#039be5"
+    change_color
+elif  [[ $1 = "--light-green" ]]; then
+    SC="#2E2E2E"
+    PC="#7cb342"
+    change_color
+elif  [[ $1 = "--lime" ]]; then
+    SC="#2E2E2E"
+    PC="#c0ca33"
+    change_color
+elif  [[ $1 = "--orange" ]]; then
+    SC="#2E2E2E"
+    PC="#fb8c00"
+    change_color
+elif  [[ $1 = "--pink" ]]; then
+    SC="#FFFFFF"
+    PC="#d81b60"
+    change_color
+elif  [[ $1 = "--purple" ]]; then
+    SC="#FFFFFF"
+    PC="#8e24aa"
+    change_color
+elif  [[ $1 = "--red" ]]; then
+    SC="#FFFFFF"
+    PC="#e53935"
+    change_color
+elif  [[ $1 = "--teal" ]]; then
+    SC="#FFFFFF"
+    PC="#00897b"
+    change_color
+elif  [[ $1 = "--yellow" ]]; then
+    SC="#2E2E2E"
+    PC="#fdd835"
+    change_color
+else
+    cat << EOF
+    No option specified, Available options:
+    --amber	--blue		--blue-gray	--brown
+    --cyan	--deep-orange	--deep-purple	--green
+    --gray	--indigo	--light-blue	--light-green
+    --lime	--orange	--pink		--purple
+    --red	--teal		--yellow
+EOF
+fi
