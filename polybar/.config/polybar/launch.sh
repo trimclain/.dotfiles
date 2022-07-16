6031e46a370463b13538e3d954384549214bc66a
Arthur McLain <mclain.it@gmail.com>
2022-07-16 11:52:35 +0200

    Autodetect correct interface for network modules

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -5,5 +5,9 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use
 # polybar-msg cmd quit
 
+# define variables for automatic wlan/eth interface detection
+export WLAN_INTERFACE=$(ip link | grep default | awk '{print $2}' | awk -F ':' '{print $1}' | grep w)
+export ETH_INTERFACE=$(ip link | grep default | awk '{print $2}' | awk -F ':' '{print $1}' | grep e)
+
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
 polybar mainbar 2>&1 | tee -a /tmp/polybar.log & disown
