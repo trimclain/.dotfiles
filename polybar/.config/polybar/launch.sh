0b887ccef9a501187253835c166e6ae196e00c8f
Arthur McLain <mclain.it@gmail.com>
2022-08-04 07:17:51 +0200

    Fix the bug caused by docker

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,8 +6,8 @@ killall -q polybar
 # polybar-msg cmd quit
 
 # define variables for automatic wlan/eth interface detection
-export WLAN_INTERFACE=$(ip link | grep default | awk '{print $2}' | awk -F ':' '{print $1}' | grep w)
-export ETH_INTERFACE=$(ip link | grep default | awk '{print $2}' | awk -F ':' '{print $1}' | grep e)
+export WLAN_INTERFACE=$(ip link | grep default | awk '{print $2}' | awk -F ':' '{print $1}' | grep wl)
+export ETH_INTERFACE=$(ip link | grep default | awk '{print $2}' | awk -F ':' '{print $1}' | grep en)
 
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
 polybar mainbar 2>&1 | tee -a /tmp/polybar.log & disown
