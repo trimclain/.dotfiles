f245145b8bb583992b29be40bb1a6a83aa3cb077
Arthur McLain <mclain.it@gmail.com>
2022-08-22 16:04:37 +0200

    feat: switch to using <C-j> and<C-k> for snippets
    :

--- nvim/.config/nvim/lua/trimclain/cmp.lua
+++ nvim/.config/nvim/lua/trimclain/cmp.lua
@@ -117,10 +117,11 @@ cmp.setup {
         ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
-            elseif luasnip.expandable() then
-                luasnip.expand()
-            elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
+            -- I have my own keybinds for this in luasnip config
+            -- elseif luasnip.expandable() then
+            --     luasnip.expand()
+            -- elseif luasnip.expand_or_jumpable() then
+            --     luasnip.expand_or_jump()
             elseif check_backspace() then
                 fallback()
             else
@@ -133,8 +134,9 @@ cmp.setup {
         ["<S-Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
+            -- I have my own keybinds for this in luasnip config
+            -- elseif luasnip.jumpable(-1) then
+            --     luasnip.jump(-1)
             else
                 fallback()
             end
