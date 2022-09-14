75b20e85754f9b4aefe048af749d364ccc76e545
Arthur McLain <mclain.it@gmail.com>
2022-09-14 16:48:15 +0200

    Update neoscroll config

--- nvim/.config/nvim/lua/trimclain/neoscroll.lua
+++ nvim/.config/nvim/lua/trimclain/neoscroll.lua
@@ -11,7 +11,7 @@ neoscroll.setup {
     stop_eof = false, -- Stop at <EOF> when scrolling downwards (default: true)
     respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
     cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-    easing_function = 'sine', -- Default easing function; options: quadratic, cubic, quartic, quintic, circular, sine
+    easing_function = "sine", -- Default easing function; options: quadratic, cubic, quartic, quintic, circular, sine
     pre_hook = nil, -- Function to run before the scrolling animation starts
     post_hook = nil, -- Function to run after the scrolling animation ends
     performance_mode = false, -- Disable "Performance Mode" on all buffers.
@@ -20,8 +20,8 @@ neoscroll.setup {
 local t = {}
 -- Syntax: t[keys] = {function, {function arguments}}
 -- Use the "sine" easing function
-t["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "350" } }
-t["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "350" } }
+t["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "50" } }
+t["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "50" } }
 -- Use the "circular" easing function
 -- t['<C-b>'] = {'scroll', {'-vim.api.nvim_win_get_height(0)', 'true', '500', [['circular']]}}
 -- t['<C-f>'] = {'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '500', [['circular']]}}
@@ -29,8 +29,8 @@ t["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "350" } }
 -- t['<C-y>'] = {'scroll', {'-0.10', 'false', '100', nil}}
 -- t['<C-e>'] = {'scroll', { '0.10', 'false', '100', nil}}
 -- When no easing function is provided the default easing function (in this case "quadratic") will be used
-t["zt"] = { "zt", { "300" } }
-t["zz"] = { "zz", { "300" } }
-t["zb"] = { "zb", { "300" } }
+t["zt"] = { "zt", { "100" } }
+t["zz"] = { "zz", { "100" } }
+t["zb"] = { "zb", { "100" } }
 
 require("neoscroll.config").set_mappings(t)
