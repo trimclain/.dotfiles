6a486bd07f866eb1de41cad3e7e9974bc9dea42b
Arthur McLain <mclain.it@gmail.com>
2022-10-07 11:39:18 +0200

    Make todo-list keymap toggle

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -1,7 +1,18 @@
 vim.opt_local.colorcolumn = "120"
 
 local opts = { noremap = true, silent = true }
-local remap = function()
-    vim.keymap.set("i", "<cr>", "<cr>- [ ] ", opts)
+
+vim.g.is_todo_list = false
+local toggle_todo_list = function()
+    if vim.g.is_todo_list == false then
+        vim.keymap.set("i", "<cr>", "<cr>- [ ] ", opts)
+        vim.g.is_todo_list = true
+        vim.notify "Enabled TODO List"
+    else
+        vim.keymap.del("i", "<cr>")
+        vim.g.is_todo_list = false
+        vim.notify "Disabled TODO List"
+    end
 end
-vim.keymap.set("n", "<leader>td", remap, opts)
+
+vim.keymap.set("n", "<leader>td", toggle_todo_list, opts)
