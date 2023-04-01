4d33c46e9851c9b7831c301443e8b79f84f434e8
Arthur McLain <mclain.it@gmail.com>
2023-04-01 14:41:38 +0200

    feat(nvim): update plugins and format the code

--- nvim/.config/nvim/lua/plugins/util.lua
+++ nvim/.config/nvim/lua/plugins/util.lua
@@ -1,18 +1,33 @@
 return {
-  -- measure startuptime
-  {
-    "dstein64/vim-startuptime",
-    -- lazy-load on a command
-    cmd = "StartupTime",
-    -- init is called during startup. Configuration for vim plugins typically should be set in an init function
-    init = function()
-      vim.g.startuptime_tries = 10
-    end,
-  },
+    -- measure startuptime
+    {
+        "dstein64/vim-startuptime",
+        -- lazy-load on a command
+        cmd = "StartupTime",
+        -- init is called during startup. Configuration for vim plugins typically should be set in an init function
+        init = function()
+            vim.g.startuptime_tries = 10
+        end,
+    },
 
-  -- library used by other plugins
-  { "nvim-lua/plenary.nvim", lazy = true },
+    -- TODO: someday?
+    -- session management
+    -- {
+    --     "folke/persistence.nvim",
+    --     event = "BufReadPre",
+    --     opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals" } },
+    --     -- stylua: ignore
+    --     keys = {
+    --         { "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
+    --         { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
+    --         { "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
+    --     },
+    -- },
 
-  -- makes some plugins dot-repeatable like leap
-  ---{ "tpope/vim-repeat", event = "VeryLazy" },
+    -- library used by other plugins
+    { "nvim-lua/plenary.nvim", lazy = true },
+
+    -- TODO: if I use leap someday
+    -- makes some plugins dot-repeatable like leap
+    ---{ "tpope/vim-repeat", event = "VeryLazy" },
 }
