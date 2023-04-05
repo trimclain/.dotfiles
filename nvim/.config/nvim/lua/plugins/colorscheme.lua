f7f6d9825e1bf7c296261c5019b5d268834010f5
Arthur McLain <mclain.it@gmail.com>
2023-04-05 10:53:35 +0200

    style(colorscheme.lua): format the code

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,23 +1,23 @@
 return {
-  -- the colorscheme should be available when starting Neovim
-  {
-    "folke/tokyonight.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme tokyonight]])
-    end,
-  },
+    -- the colorscheme should be available when starting Neovim
+    {
+        "folke/tokyonight.nvim",
+        lazy = false, -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+            -- load the colorscheme here
+            vim.cmd.colorscheme("tokyonight")
+        end,
+    },
 
-  -- local plugins need to be explicitly configured with dir
-  ---{ dir = "~/projects/secret.nvim" },
+    -- local plugins need to be explicitly configured with dir
+    ---{ dir = "~/projects/secret.nvim" },
 
-  -- you can use a custom url to fetch a plugin
-  ---{ url = "git@github.com:folke/noice.nvim.git" },
+    -- you can use a custom url to fetch a plugin
+    ---{ url = "git@github.com:folke/noice.nvim.git" },
 
-  -- local plugins can also be configure with the dev option.
-  -- This will use {config.dev.path}/noice.nvim/ instead of fetching it from Github
-  -- With the dev option, you can easily switch between the local and installed version of a plugin
-  ---{ "folke/noice.nvim", dev = true },
+    -- local plugins can also be configure with the dev option.
+    -- This will use {config.dev.path}/noice.nvim/ instead of fetching it from Github
+    -- With the dev option, you can easily switch between the local and installed version of a plugin
+    ---{ "folke/noice.nvim", dev = true },
 }
