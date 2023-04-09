7056bd88af08254fc8ac9cdf381affb3e86b5730
Arthur McLain <mclain.it@gmail.com>
2023-04-09 21:34:43 +0200

    feat(alpha.nvim): update keymap for find text

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -369,7 +369,7 @@ return {
                 dashboard.button("f", " " .. " Find file", ":Telescope find_files<CR>"),
                 dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert<CR>"),
                 dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles<CR>"),
-                dashboard.button("g", " " .. " Find text", ":Telescope live_grep<CR>"),
+                dashboard.button("s", " " .. " Find string", ":Telescope live_grep<CR>"),
                 dashboard.button("c", " " .. " Config", ":e $MYVIMRC | cd ~/.config/nvim<CR>"),
                 dashboard.button("l", "󰒲 " .. " Lazy", ":Lazy<CR>"),
                 dashboard.button("q", " " .. " Quit", ":qa<CR>"),
