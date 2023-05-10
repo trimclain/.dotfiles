e5dffe60048600ca1542717f2ff1efb5189594dd
Arthur McLain <mclain.it@gmail.com>
2023-05-10 21:31:16 +0200

    feat(polybar): add multimonitor support (wip)

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -5,12 +5,30 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use
 # polybar-msg cmd quit
 
+activeMonitors=$(polybar -M | cut -d ':' -f 1)
+monitors=()
+for display in $activeMonitors; do
+    monitors+=($display)
+done
+export MAIN_MONITOR=${monitors[0]}
+export SECOND_MONITOR=${monitors[1]}
+
 # define variables for automatic wlan/eth interface detection
 export WLAN_INTERFACE=$(ip link | grep default | awk '{print $2}' | awk -F ':' '{print $1}' | grep wl)
 export ETH_INTERFACE=$(ip link | grep default | awk '{print $2}' | awk -F ':' '{print $1}' | grep en)
 
 # Launch the correct bar depending on display dimensions
-if [[ $(xdpyinfo | awk '/dimensions/{print $2}') == "1366x768" ]]; then
+# Two 1920x1080 monitors
+if [[ $(xdpyinfo | awk '/dimensions/{print $2}') == "3840x1080" ]]; then
+    polybar --config=$HOME/.config/polybar/config.ini mainbar 2>&1 | tee -a /tmp/polybar.log & disown
+    if [[ -n $SECOND_MONITOR ]]; then
+        polybar --config=$HOME/.config/polybar/config.ini mainbar2 2>&1 | tee -a /tmp/polybar.log & disown
+    fi
+
+# SOMEDAY:
+# xdpyinfo only gives the sum, so options : 3840 - 2 normals, 3286 -- one 1366 and one normal, normal, small
+
+elif [[ $(xdpyinfo | awk '/dimensions/{print $2}') == "1366x768" ]]; then
     # Launch Polybar, using default config location ~/.config/polybar/config.ini
     # Since on ubuntu I have version 3.5.7, which doesn't default to config.ini yet, specify config variable
     polybar --config=$HOME/.config/polybar/config.ini minibar 2>&1 | tee -a /tmp/polybar.log & disown
