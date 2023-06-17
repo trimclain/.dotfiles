815968b7da60afa676662b8bd4a6f8c9345908ec
Arthur McLain <mclain.it@gmail.com>
2023-06-18 01:42:42 +0200

    chore(nvim): update config

--- nvim/.config/nvim/lua/plugins/coding.lua
+++ nvim/.config/nvim/lua/plugins/coding.lua
@@ -340,8 +340,23 @@ return {
     -- close tags using treesitter
     {
         "windwp/nvim-ts-autotag",
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
         event = "InsertEnter",
     },
+
+    -- I use snippets instead, but in case I want here's the link
+    -- wisely add "end" in Ruby, Vimscript, Lua, etc
+    -- {
+    --     "RRethy/nvim-treesitter-endwise",
+    --     dependencies = { "nvim-treesitter/nvim-treesitter" },
+    -- },
+
+    -- I might find a use fo this one later
+    -- tab out from parentheses, quotes, and similar contexts
+    -- {
+    --     "abecodes/tabout.nvim",
+    --     dependencies = { "nvim-treesitter/nvim-treesitter" },
+    -- },
 
     -- comments
     {
