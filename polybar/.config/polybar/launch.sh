7e55e53b8888b03c5da2e9f005aca592a831acb8
Arthur McLain <mclain.it@gmail.com>
2023-09-17 15:04:37 +0200

    chore(polybar): update config

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -14,15 +14,17 @@ export MAIN_MONITOR=${monitors[0]}
 export SECOND_MONITOR=${monitors[1]}
 
 # define variables for automatic wlan/eth interface detection
-export WLAN_INTERFACE=$(ip link | grep default | awk '{print $2}' | awk -F ':' '{print $1}' | grep wl)
-export ETH_INTERFACE=$(ip link | grep default | awk '{print $2}' | awk -F ':' '{print $1}' | grep en)
+export WLAN_INTERFACE
+WLAN_INTERFACE=$(ip link | grep default | awk '{print $2}' | awk -F ':' '{print $1}' | grep wl)
+export ETH_INTERFACE
+ETH_INTERFACE=$(ip link | grep default | awk '{print $2}' | awk -F ':' '{print $1}' | grep en)
 
 # Launch the correct bar depending on display dimensions
 # Two 1920x1080 monitors
 if [[ $(xdpyinfo | awk '/dimensions/{print $2}') == "3840x1080" ]]; then
-    polybar --config=$HOME/.config/polybar/config.ini mainbar 2>&1 | tee -a /tmp/polybar.log & disown
+    polybar --config="$HOME/.config/polybar/config.ini" mainbar 2>&1 | tee -a /tmp/polybar.log & disown
     if [[ -n $SECOND_MONITOR ]]; then
-        polybar --config=$HOME/.config/polybar/config.ini mainbar2 2>&1 | tee -a /tmp/polybar.log & disown
+        polybar --config="$HOME/.config/polybar/config.ini" mainbar2 2>&1 | tee -a /tmp/polybar.log & disown
     fi
 
 # SOMEDAY:
@@ -31,7 +33,7 @@ if [[ $(xdpyinfo | awk '/dimensions/{print $2}') == "3840x1080" ]]; then
 elif [[ $(xdpyinfo | awk '/dimensions/{print $2}') == "1366x768" ]]; then
     # Launch Polybar, using default config location ~/.config/polybar/config.ini
     # Since on ubuntu I have version 3.5.7, which doesn't default to config.ini yet, specify config variable
-    polybar --config=$HOME/.config/polybar/config.ini minibar 2>&1 | tee -a /tmp/polybar.log & disown
+    polybar --config="$HOME/.config/polybar/config.ini" minibar 2>&1 | tee -a /tmp/polybar.log & disown
 else
-    polybar --config=$HOME/.config/polybar/config.ini mainbar 2>&1 | tee -a /tmp/polybar.log & disown
+    polybar --config="$HOME/.config/polybar/config.ini" mainbar 2>&1 | tee -a /tmp/polybar.log & disown
 fi
