acce12b19e7e28f3aee453a1b6050c5728d082cc
Arthur McLain <mclain.it@gmail.com>
2023-10-04 20:16:46 +0200

    feat(nvim): add dressing.nvim

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -41,6 +41,23 @@ return {
         end,
     },
 
+    -- better vim.ui
+    {
+        "stevearc/dressing.nvim",
+        lazy = true,
+        init = function()
+            ---@diagnostic disable-next-line: duplicate-set-field
+            vim.ui.select = function(...)
+                require("lazy").load({ plugins = { "dressing.nvim" } })
+                return vim.ui.select(...)
+            end
+            ---@diagnostic disable-next-line: duplicate-set-field
+            vim.ui.input = function(...)
+                require("lazy").load({ plugins = { "dressing.nvim" } })
+                return vim.ui.input(...)
+            end
+        end,
+    },
     -- bufferline
     {
         "akinsho/bufferline.nvim",
@@ -593,7 +610,4 @@ return {
 
     -- ui components
     { "MunifTanjim/nui.nvim", lazy = true },
-
-    -- TODO:
-    -- { "stevearc/dressing.nvim", lazy = true },
 }
