5434799b16a7527d1e22f40630ff8dd0bdbe594b
Arthur McLain <mclain.it@gmail.com>
2023-10-07 03:50:45 +0200

    feat(wezterm): update config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -16,7 +16,7 @@
 -- Why I stopped using wezterm for now:
 -- 1. After I do `nohup xdg-open "$filename" &>/dev/null &` some number of times, my PC freezes when I exit wezterm
 --      There might be fixed, if I set "no_hup and no_check_jobs" options in zshrc
--- 2. When changing the font size, it also changes the windows size
+-- 2. When changing the font size, it also changes the windows size (can be fixed by maximizing the window)
 
 -- As soon as these things get fixed, I'm coming back to it
 -- Reasons:
@@ -26,7 +26,6 @@
 -- 4. Changes in config apply instantly in same window
 -- 5. Supports sixel AND kitty image format
 -- 5. Written in rust (btw)
-
 
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
@@ -68,17 +67,30 @@ local fonts = {
     "DejaVuSansM Nerd Font",
     "JetBrains Mono", -- pre-installed
 }
-config.font = wezterm.font_with_fallback({ fonts[1], fonts[#fonts - 1], fonts[#fonts] })
+config.font = wezterm.font_with_fallback({ fonts[2], fonts[#fonts - 1], fonts[#fonts] })
 config.font_size = 13.0
 
-config.window_background_opacity = 0.85
+-- local cascadia_code = {
+--     { family = "Cascadia Code", weight = "Light", stretch = "Normal", style = "Normal" },
+--     { family = "Cascadia Code", weight = "Light", stretch = "Normal", style = "Italic" },
+--     { family = "Cascadia Code", weight = "DemiBold", stretch = "Normal", style = "Normal" },
+--     { family = "Cascadia Code", weight = "DemiBold", stretch = "Normal", style = "Italic" },
+-- }
+-- config.font = wezterm.font_with_fallback(cascadia_code)
+
+-- config.window_background_opacity = 0.85
 -- config.color_scheme = "Catppuccin Macchiato"
+-- config.color_scheme = "GitHub Dark"
+
+-- Set background to same color as neovim
+config.colors = {}
+config.colors.background = "#21262d"
 
 -- https://github.com/samuelngs/apple-emoji-linux/releases/tag/v16.4
 -- local emoji_fonts={ "Apple Color Emoji", "Joypixels", "Twemoji", "Noto Color Emoji", "Noto Emoji" }
 -- config.font = wezterm.font_with_fallback({ fonts[1], emoji_fonts[1], emoji_fonts[2] })
 
--- https://www.monolisa.dev/playground
+-- https://learn.microsoft.com/en-us/typography/opentype/spec/featurelist
 -- config.harfbuzz_features = { "calt", "liga", "zero", "-ss01", "ss02", "-ss03", "ss04", "ss05", "ss06", "ss07", "-ss08", "-ss09" }
 -- to disable something add =0 to the end or - at the beginning
 config.harfbuzz_features = {
