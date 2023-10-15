d357d54644cac645834d5a248cfa466834a49a9c
Arthur McLain <mclain.it@gmail.com>
2023-10-15 02:42:38 +0200

    chore(treesitter): update config

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -29,7 +29,18 @@ return {
         -- },
         ---@type TSConfig
         opts = {
-            highlight = { enable = true },
+            highlight = {
+                enable = true,
+                -- disable slow treesitter highlight for large files
+                disable = function(lang, buf)
+                    -- return lang == "cpp" and vim.api.nvim_buf_line_count(bufnr) > 50000
+                    local max_filesize = 100 * 1024 -- 100 KB
+                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                    if ok and stats and stats.size > max_filesize then
+                        return true
+                    end
+                end,
+            },
             indent = {
                 enable = true,
                 disable = { "julia" }, -- sadly broken right now
