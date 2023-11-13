594e52d16ebb19acd1dfd9dbbd5ade5e3013230c
Arthur McLain <mclain.it@gmail.com>
2023-11-13 19:23:25 +0100

    feat(todo-comments): enable search in hidden folders

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -667,6 +667,21 @@ return {
                 NOTE = { icon = " ", color = "hint", alt = { "INFO", "IDEA" } },
                 TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
             },
+            search = {
+                command = "rg",
+                args = {
+                    "--color=never",
+                    "--no-heading",
+                    "--with-filename",
+                    "--line-number",
+                    "--column",
+                    "--hidden", -- search hidden files/directories
+                },
+                -- regex that will be used to match keywords.
+                -- don't replace the (KEYWORDS) placeholder
+                pattern = [[\b(KEYWORDS):]], -- ripgrep regex
+                -- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
+            },
         },
     },
 
