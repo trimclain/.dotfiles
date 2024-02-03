496650acde139d1a14c49270ce48e04f303f9c93
Arthur McLain <mclain.it@gmail.com>
2024-02-03 08:51:12 +0100

    chore(waybar): update launch.sh

--- waybar/.config/waybar/launch.sh
+++ waybar/.config/waybar/launch.sh
@@ -21,25 +21,12 @@ done
 MAIN_RES=${resolutions[0]}
 SECOND_RES=${resolutions[1]}
 
-# # define variables for automatic wlan/eth interface detection
-# export WLAN_INTERFACE
-# WLAN_INTERFACE=$(ip link | awk '/default/ {split($2, a, ":"); print a[1]}' | grep wl)
-# export ETH_INTERFACE
-# ETH_INTERFACE=$(ip link | awk '/default/ {split($2, a, ":"); print a[1]}' | grep en)
-
-# TODO: multimonitor bar
-# # Launch the correct bar depending on display dimensions
-# # Two 1920x1080 monitors
-# if [[ $(xdpyinfo | awk '/dimensions/{print $2}') == "3840x1080" ]]; then
-#     polybar --config="$HOME/.config/polybar/config.ini" mainbar 2>&1 | tee -a /tmp/polybar.log & disown
-#     if [[ -n $SECOND_MONITOR ]]; then
-#         polybar --config="$HOME/.config/polybar/config.ini" mainbar2 2>&1 | tee -a /tmp/polybar.log & disown
-#     fi
-
-# # SOMEDAY:
-# # xdpyinfo only gives the sum, so options : 3840 - 2 normals, 3286 -- one 1366 and one normal, normal, small
-
+echo "$MAIN_RES"
 if [[ "$MAIN_RES" == "1366x768" ]]; then
+    # TODO: to launch waybar on specific monitor and use env variable:
+    # exec_always "VARIABLE_FOR_WAYBAR=$mySwayVariable envsubst < ~/.config/waybar/config.jsonc > /tmp/waybar.jsonc; pkill waybar; exec waybar -c /tmp/waybar.jsonc"
+    # and in waybar conf:
+    # "output": "${VARIABLE_FOR_WAYBAR}",
     waybar -c "$HOME/.config/waybar/mini/config" \
         -s "$HOME/.config/waybar/mini/style.css" 2>&1 | tee -a /tmp/waybar.log & disown
 else
