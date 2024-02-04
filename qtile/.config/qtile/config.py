547991b18e64cdcce2ae52dac9edd85adc1d48e7
Arthur McLain <mclain.it@gmail.com>
2024-02-05 00:57:36 +0100

    chore: make yad boxes always float

--- qtile/.config/qtile/config.py
+++ qtile/.config/qtile/config.py
@@ -731,6 +731,7 @@ floating_layout = layout.Floating(
         Match(wm_class="makebranch"),  # gitk
         Match(wm_class="maketag"),  # gitk
         Match(wm_class="ssh-askpass"),  # ssh-askpass
+        Match(wm_class="Yad"),  # yad boxes
         Match(title="branchdialog"),  # gitk
         Match(title="pinentry"),  # GPG key password entry
     ], **floating_layout_defaults
