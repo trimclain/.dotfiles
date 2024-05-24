7323b9377749197f17fd8b963fbd4318665951eb
Arthur McLain <mclain.it@gmail.com>
2024-05-24 22:17:58 +0200

    feat(nvim): add debugging (wip)

---
+++ nvim/.config/nvim/lua/plugins/debug.lua
@@ -0,0 +1,11 @@
+return {
+    -- {
+    --     "mfussenegger/nvim-dap",
+    --     dependencies = {
+    --         "rcarriga/nvim-dap-ui",
+    --         "theHamsta/nvim-dap-virtual-text",
+    --         "nvim-telescope/telescope-dap.nvim",
+    --         "mfussenegger/nvim-dap-python",
+    --     },
+    -- },
+}
