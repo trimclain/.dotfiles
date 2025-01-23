9f10864526f10d014e06d319ed3a55fc4af6e040
Arthur McLain <mclain.it@gmail.com>
2025-01-23 18:57:41 +0100

    chore(nvim): update config

--- nvim/.config/nvim/lua/plugins/util.lua
+++ nvim/.config/nvim/lua/plugins/util.lua
@@ -131,4 +131,55 @@ return {
     --     { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll backward", mode = {"i", "n", "s"}},
     --   },
     -- },
+
+    -- from editor.lua
+    -- Alternative: "stevearc/quicker.nvim"
+    -- TODO: someday
+    -- better diagnostics list and others
+    -- {
+    --     "folke/trouble.nvim",
+    --     cmd = { "TroubleToggle", "Trouble" },
+    --     opts = { use_diagnostic_signs = true },
+    --     keys = {
+    --         { "<leader>xx", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Document Diagnostics (Trouble)" },
+    --         { "<leader>xX", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics (Trouble)" },
+    --         { "<leader>xL", "<cmd>TroubleToggle loclist<cr>", desc = "Location List (Trouble)" },
+    --         { "<leader>xQ", "<cmd>TroubleToggle quickfix<cr>", desc = "Quickfix List (Trouble)" },
+    --         {
+    --             "[q",
+    --             function()
+    --                 if require("trouble").is_open() then
+    --                     require("trouble").previous({ skip_groups = true, jump = true })
+    --                 else
+    --                     vim.cmd.cprev()
+    --                 end
+    --             end,
+    --             desc = "Previous trouble/quickfix item",
+    --         },
+    --         {
+    --             "]q",
+    --             function()
+    --                 if require("trouble").is_open() then
+    --                     require("trouble").next({ skip_groups = true, jump = true })
+    --                 else
+    --                     vim.cmd.cnext()
+    --                 end
+    --             end,
+    --             desc = "Next trouble/quickfix item",
+    --         },
+    --     },
+    -- },
+
+    -- auto remove search highlight and rehighlight when using n or N
+    -- {
+    --     "nvimdev/hlsearch.nvim",
+    --     config = true,
+    -- },
+
+    -- incsearch for :linenum<cr>
+    -- {
+    --     "nacro90/numb.nvim",
+    --     event = "BufReadPost",
+    --     config = true,
+    -- },
 }
---
+++ nvim/.config/nvim/lua/plugins/zen-mode.lua
@@ -0,0 +1,29 @@
+return {
+    {
+        "folke/zen-mode.nvim",
+        -- dependencies = {
+        --     -- twilight
+        --     {
+        --         "folke/twilight.nvim",
+        --         opts = {
+        --             alpha = 0.25, -- amount of dimming
+        --         },
+        --     },
+        -- },
+        cmd = "ZenMode",
+        -- stylua: ignore
+        keys = {
+            { "<leader>z", function() require("zen-mode").toggle() end, desc = "Toggle Zen Mode" }
+        },
+        opts = {
+            -- this will change the font size on kitty when in zen mode
+            -- to make this work, you need to set the following kitty options:
+            -- - allow_remote_control socket-only
+            -- - listen_on unix:/tmp/kitty
+            kitty = {
+                enabled = false,
+                font = "+4", -- font size increment
+            },
+        },
+    },
+}
