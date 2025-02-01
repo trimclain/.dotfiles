8714df0ec620bb666e65aa63911373b469c574c8
Arthur McLain <mclain.it@gmail.com>
2025-02-01 02:56:24 +0100

    feat: switch to neorg stable
    Yeah it's my fork of pinned commit and I ship parsers too

--- nvim/.config/nvim/lua/plugins/neorg.lua
+++ nvim/.config/nvim/lua/plugins/neorg.lua
@@ -1,10 +1,10 @@
 -- advanced note taking, project/task management
 return {
     {
-        "nvim-neorg/neorg",
-        -- TODO: repin
-        commit = "086891d", -- it updates too fast for me to care
-        build = ":Neorg sync-parsers",
+        "trimclain/neorg",
+        -- "nvim-neorg/neorg",
+        -- commit = "086891d", -- no luarocks, thanks
+        -- build = ":Neorg sync-parsers", -- this doesn't work with pinned version. I'll do this manually.
         ft = "norg",
         dependencies = {
             "plenary.nvim",
@@ -19,19 +19,29 @@ return {
                             icon_preset = "basic", -- "basic" (default), "diamond", "varied"
                         },
                     },
-                    -- ["core.dirman"] = { -- Manages Neorg workspaces
-                    --     config = {
-                    --         workspaces = {
-                    --             notes = "~/notes",
-                    --         },
-                    --     },
-                    -- },
-                    -- https://github.com/nvim-neorg/neorg/wiki/Completion#configuration
-                    -- ["core.completion"] = {
-                    --     config = { engine = "nvim-cmp" },
-                    -- },
                 },
             })
+
+            -- TODO: test on Windows
+
+            -- Install neorg parsers
+            local function generate_paths(base_path)
+                return {
+                    norg_parser = base_path .. "parser/norg.so",
+                    norg_meta_parser = base_path .. "parser/norg_meta.so",
+                    norg_parser_revision = base_path .. "parser-info/norg.revision",
+                    norg_meta_parser_revision = base_path .. "parser-info/norg_meta.revision",
+                }
+            end
+            local src = generate_paths(vim.fn.stdpath("data") .. "/lazy/neorg/")
+            local dest = generate_paths(vim.fn.stdpath("data") .. "/lazy/nvim-treesitter/")
+
+            for key, value in pairs(dest) do
+                if vim.fn.filereadable(value) == 0 then
+                    -- vim.fn.delete(dest[key])
+                    vim.uv.fs_copyfile(src[key], value)
+                end
+            end
 
             vim.keymap.set("n", "<leader>nc", "<cmd>Neorg toggle-concealer<cr>", { desc = "Toggle Neorg Concealer" })
         end,
