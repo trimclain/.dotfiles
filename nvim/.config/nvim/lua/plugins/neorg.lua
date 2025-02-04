5787ea8d8dabca3de31681efbda9450f98e34da3
Arthur McLain <mclain.it@gmail.com>
2025-02-04 05:43:40 +0100

    fix(nvim): update neorg to the latest version

--- nvim/.config/nvim/lua/plugins/neorg.lua
+++ nvim/.config/nvim/lua/plugins/neorg.lua
@@ -1,14 +1,18 @@
 -- advanced note taking, project/task management
 return {
     {
-        "trimclain/neorg",
-        -- "nvim-neorg/neorg",
-        -- commit = "086891d", -- no luarocks, thanks
-        -- build = ":Neorg sync-parsers", -- this doesn't work with pinned version. I'll do this manually.
+        "nvim-neorg/neorg",
+        version = "*",
         ft = "norg",
         dependencies = {
-            "plenary.nvim",
             "nvim-treesitter",
+
+            -- no need for luarocks, I'll do it myself
+            "nui.nvim",
+            "plenary.nvim",
+            "nvim-neotest/nvim-nio",
+            "nvim-neorg/lua-utils.nvim",
+            "pysan3/pathlib.nvim",
         },
         config = function()
             require("neorg").setup({
@@ -22,26 +26,8 @@ return {
                 },
             })
 
-            -- TODO: test on Windows
-
-            -- Install neorg parsers
-            local function generate_paths(base_path)
-                return {
-                    norg_parser = base_path .. "parser/norg.so",
-                    norg_meta_parser = base_path .. "parser/norg_meta.so",
-                    norg_parser_revision = base_path .. "parser-info/norg.revision",
-                    norg_meta_parser_revision = base_path .. "parser-info/norg_meta.revision",
-                }
-            end
-            local src = generate_paths(vim.fn.stdpath("data") .. "/lazy/neorg/")
-            local dest = generate_paths(vim.fn.stdpath("data") .. "/lazy/nvim-treesitter/")
-
-            for key, value in pairs(dest) do
-                if vim.fn.filereadable(value) == 0 then
-                    -- vim.fn.delete(dest[key])
-                    vim.uv.fs_copyfile(src[key], value)
-                end
-            end
+            vim.wo.foldlevel = 99
+            vim.wo.conceallevel = 2
 
             vim.keymap.set("n", "<leader>nc", "<cmd>Neorg toggle-concealer<cr>", { desc = "Toggle Neorg Concealer" })
         end,
