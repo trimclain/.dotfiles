e7d33767ed1b97fa99acd07f57d3b9fbef003178
Arthur McLain <mclain.it@gmail.com>
2025-02-28 10:03:48 +0100

    feat(nvim): add keymap to toggle markdown checkbox

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -21,5 +21,15 @@ local toggle_inserting_checkboxes = function()
 end
 vim.keymap.set("n", "<leader>td", toggle_inserting_checkboxes, opts)
 
+--- Check or uncheck the checkbox in current line
+local toggle_checkbox = function()
+    local line = vim.api.nvim_get_current_line()
+    if vim.startswith(line, "- [ ] ") then
+        vim.api.nvim_set_current_line("- [x] " .. string.sub(line, 7))
+    elseif vim.startswith(line, "- [x] ") then
+        vim.api.nvim_set_current_line("- [ ] " .. string.sub(line, 7))
+    end
+end
+vim.keymap.set("n", "<C-s>", toggle_checkbox, opts)
 
 vim.keymap.set("i", "--", "—", opts)
