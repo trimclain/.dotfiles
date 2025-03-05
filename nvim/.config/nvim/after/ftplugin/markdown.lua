abe234a1bdba41247b93d9bed28f24280359916e
Arthur McLain <mclain.it@gmail.com>
2025-03-05 21:59:06 +0100

    feat(nvim): update markdown toggle checkboxes
    Now works with tabbed out checkboxes and list items.

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -21,16 +21,43 @@ local toggle_inserting_checkboxes = function()
 end
 vim.keymap.set("n", "<leader>td", toggle_inserting_checkboxes, opts)
 
+---Detects if a string starts with whitespaces and returns the length of them.
+---@param str string The string to check.
+---@return integer|nil The length of the leading whitespaces, nil if no leading whitespaces.
+local function get_leading_whitespaces_length(str)
+    local length = string.len(str)
+    for i = 1, length do
+        local char = string.sub(str, i, i)
+        -- find a non-whitespace character
+        if char:match("%S") then
+            return i - 1
+        end
+    end
+    -- if the string contains only whitespaces or is empty
+    return nil
+end
+
 --- Check/uncheck the checkbox or convert the list item into the checkbox in current line
 local toggle_checkbox = function()
     local line = vim.api.nvim_get_current_line()
-    if vim.startswith(line, "- [ ] ") then
-        vim.api.nvim_set_current_line("- [x] " .. string.sub(line, 7))
-    elseif vim.startswith(line, "- [x] ") then
-        vim.api.nvim_set_current_line("- [ ] " .. string.sub(line, 7))
-    elseif vim.startswith(line, "- ") then
-        vim.api.nvim_set_current_line("- [ ] " .. string.sub(line, 3))
+    local leading_whitespace_length = get_leading_whitespaces_length(line)
+
+    if leading_whitespace_length == nil then
+        return
     end
+
+    local trimmed_line = string.sub(line, leading_whitespace_length + 1)
+    if vim.startswith(trimmed_line, "- [ ] ") then
+        trimmed_line = "- [x] " .. string.sub(trimmed_line, 7)
+    elseif vim.startswith(trimmed_line, "- [x] ") then
+        trimmed_line = "- [ ] " .. string.sub(trimmed_line, 7)
+    elseif vim.startswith(trimmed_line, "- ") then
+        trimmed_line = "- [ ] " .. string.sub(trimmed_line, 3)
+    else
+        return
+    end
+
+    vim.api.nvim_set_current_line(string.rep(" ", leading_whitespace_length) .. trimmed_line)
 end
 vim.keymap.set("n", "<C-s>", toggle_checkbox, opts)
 
