c8de1ca972776f7d97082aec9f8c88c1f2f0221a
Arthur McLain <mclain.it@gmail.com>
2025-06-27 16:23:46 +0200

    chore(yazi): update config

--- yazi/.config/yazi/plugins/smart-enter.yazi/main.lua
+++ yazi/.config/yazi/plugins/smart-enter.yazi/main.lua
@@ -1,6 +1,15 @@
 --- @since 25.5.31
 --- @sync entry
 
+-- Credit: https://github.com/yazi-rs/plugins/blob/main/smart-enter.yazi
+
+-- To open multiple selected files, add this to your `~/.config/yazi/init.lua`:
+-- ```lua
+-- require("smart-enter"):setup {
+--     open_multi = true,
+-- }
+-- ```
+
 local function setup(self, opts)
     self.open_multi = opts.open_multi
 end
