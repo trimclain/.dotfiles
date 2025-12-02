00b22b20c19d04a3965670926b710aa7a83c7b2a
Arthur McLain <mclain.it@gmail.com>
2025-12-02 17:04:33 +0100

    chore(waybar): use one style.css for all bars

--- waybar/.config/waybar/launch.sh
+++ waybar/.config/waybar/launch.sh
@@ -21,14 +21,13 @@ done
 MAIN_RES=${resolutions[0]}
 SECOND_RES=${resolutions[1]}
 
-echo "$MAIN_RES"
 if [[ "$MAIN_RES" == "1366x768" ]]; then
     # TODO: to launch waybar on specific monitor and use env variable:
     # exec_always "VARIABLE_FOR_WAYBAR=$mySwayVariable envsubst < ~/.config/waybar/config.jsonc > /tmp/waybar.jsonc; pkill waybar; exec waybar -c /tmp/waybar.jsonc"
     # and in waybar conf:
     # "output": "${VARIABLE_FOR_WAYBAR}",
     waybar -c "$HOME/.config/waybar/mini/config.jsonc" \
-        -s "$HOME/.config/waybar/mini/style.css" 2>&1 | tee -a /tmp/waybar.log & disown
+        -s "$HOME/.config/waybar/style.css" 2>&1 | tee -a /tmp/waybar.log & disown
 else
     waybar 2>&1 | tee -a /tmp/waybar.log & disown
 fi
