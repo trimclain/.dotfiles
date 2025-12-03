04a4accd4b01fd091ff6a9eb5a70f7b10a2d07ed
Arthur McLain <mclain.it@gmail.com>
2025-12-03 13:05:41 +0100

    feat(waybar): add config for multiple monitors

--- waybar/.config/waybar/launch.sh
+++ waybar/.config/waybar/launch.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# TODO: finish this
-
 # Terminate already running bar instances
 killall -q waybar
 
@@ -11,7 +9,7 @@ for display in $activeMonitors; do
     monitors+=("$display")
 done
 MAIN_MONITOR=${monitors[0]}
-SECOND_MONITOR=${monitors[1]}
+# SECOND_MONITOR=${monitors[1]}
 
 resolutions=()
 monitorResolutions=$(hyprctl monitors | awk -F@ '/ at / {print $1}')
@@ -19,15 +17,18 @@ for res in $monitorResolutions; do
     resolutions+=("$res")
 done
 MAIN_RES=${resolutions[0]}
-SECOND_RES=${resolutions[1]}
+
+WAYBAR_CONFIG=~/.config/waybar/config.jsonc
+WAYBAR_STYLE=~/.config/waybar/style.css
+TEMP_CONFIG=/tmp/waybar.jsonc
+LOG_FILE=/tmp/waybar.log
 
 if [[ "$MAIN_RES" == "1366x768" ]]; then
-    # TODO: to launch waybar on specific monitor and use env variable:
-    # exec_always "VARIABLE_FOR_WAYBAR=$mySwayVariable envsubst < ~/.config/waybar/config.jsonc > /tmp/waybar.jsonc; pkill waybar; exec waybar -c /tmp/waybar.jsonc"
-    # and in waybar conf:
-    # "output": "${VARIABLE_FOR_WAYBAR}",
-    waybar -c "$HOME/.config/waybar/mini/config.jsonc" \
-        -s "$HOME/.config/waybar/style.css" 2>&1 | tee -a /tmp/waybar.log & disown
-else
-    waybar 2>&1 | tee -a /tmp/waybar.log & disown
+    # TODO: add output variables
+    WAYBAR_CONFIG=~/.config/waybar/mini/config.jsonc
 fi
+
+# Primary Bar
+MAIN_MONITOR=$MAIN_MONITOR envsubst < $WAYBAR_CONFIG > $TEMP_CONFIG; \
+    waybar -c $TEMP_CONFIG -s $WAYBAR_STYLE 2>&1 \
+    | tee -a $LOG_FILE & disown
