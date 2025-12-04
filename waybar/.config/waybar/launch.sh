152cb188aafe3c03763d350c38cdb6ad85f2bc76
Arthur McLain <mclain.it@gmail.com>
2025-12-04 12:49:42 +0100

    fix(waybar): update monitor detection on startup

--- waybar/.config/waybar/launch.sh
+++ waybar/.config/waybar/launch.sh
@@ -3,20 +3,35 @@
 # Terminate already running bar instances
 killall -q waybar
 
-activeMonitors=$(hyprctl monitors | awk '$1 == "Monitor"{print $2}')
+connectedOutputs=$(hyprctl monitors all | awk '$1 == "Monitor"{print $2}')
 monitors=()
-for display in $activeMonitors; do
+for display in $connectedOutputs; do
     monitors+=("$display")
 done
-MAIN_MONITOR=${monitors[0]}
-# SECOND_MONITOR=${monitors[1]}
 
 resolutions=()
-monitorResolutions=$(hyprctl monitors | awk -F@ '/ at / {print $1}')
+monitorResolutions=$(hyprctl monitors all | awk -F@ '/ at / {print $1}')
 for res in $monitorResolutions; do
     resolutions+=("$res")
 done
-MAIN_RES=${resolutions[0]}
+
+# External monitor is not always the first one
+if [[ ${monitors[0]} == *HDMI* ]]; then
+    INTERNAL=${monitors[1]}
+    EXTERNAL=${monitors[0]}
+    MAIN_RES=${resolutions[0]}
+else
+    INTERNAL=${monitors[0]}
+    EXTERNAL=${monitors[1]}
+    MAIN_RES=${resolutions[1]}
+fi
+
+# If no external monitor connected, fallback to internal
+MAIN_MONITOR=$EXTERNAL
+if [[ -z $MAIN_MONITOR ]]; then
+    MAIN_MONITOR=$INTERNAL
+    MAIN_RES=${resolutions[0]}
+fi
 
 WAYBAR_CONFIG=~/.config/waybar/config.jsonc
 WAYBAR_STYLE=~/.config/waybar/style.css
@@ -24,7 +39,6 @@ TEMP_CONFIG=/tmp/waybar.jsonc
 LOG_FILE=/tmp/waybar.log
 
 if [[ "$MAIN_RES" == "1366x768" ]]; then
-    # TODO: add output variables
     WAYBAR_CONFIG=~/.config/waybar/mini/config.jsonc
 fi
 
