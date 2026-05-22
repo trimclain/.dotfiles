bd127276ecb8cfde3a44fe719649e9430a4eccbf
Arthur McLain <mclain.it@gmail.com>
2026-05-22 08:09:29 +0200

    chore(awesome): disable mouse scrolling in the taglist widget

--- awesome/.config/awesome/ui/statusbar.lua
+++ awesome/.config/awesome/ui/statusbar.lua
@@ -22,13 +22,13 @@ local taglist_buttons = gears.table.join(
         if client.focus then
             client.focus:toggle_tag(t)
         end
-    end),
-    awful.button({}, 4, function(t)
-        awful.tag.viewnext(t.screen)
-    end),
-    awful.button({}, 5, function(t)
-        awful.tag.viewprev(t.screen)
     end)
+    -- awful.button({}, 4, function(t)
+    --     awful.tag.viewprev(t.screen)
+    -- end),
+    -- awful.button({}, 5, function(t)
+    --     awful.tag.viewnext(t.screen)
+    -- end)
 )
 
 local tasklist_buttons = gears.table.join(
