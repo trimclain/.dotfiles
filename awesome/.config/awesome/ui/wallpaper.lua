9e1ac907eb189361bcdf490e24a61ee8de03d06f
Arthur McLain <mclain.it@gmail.com>
2026-05-22 04:20:07 +0200

    feat(awesome): update ui.wallpaper.setup to accept wallpaper as optional argument

--- awesome/.config/awesome/ui/wallpaper.lua
+++ awesome/.config/awesome/ui/wallpaper.lua
@@ -1,18 +1,26 @@
 local gears = require("gears") -- Utilities such as color parsing and objects
 local beautiful = require("beautiful") -- Theme handling library
 
+local utils = require("core.utils")
+
 local M = {}
 
-function M.setup(s)
-    -- TODO: it's better to pass the wallpaper to make it more modular
-    -- This would also remove the beautiful call which I want to achieve anyway
-    if beautiful.wallpaper then
-        local wallpaper = beautiful.wallpaper
-        if type(wallpaper) == "function" then
-            wallpaper = wallpaper(s)
-        end
-        gears.wallpaper.maximized(wallpaper, s, true)
+---@class awesome.screen
+---@field index integer
+
+--- Set the wallpaper using gears.wallpaper
+---@param screen awesome.screen screen on which to set the wallpaper
+---@param wallpaper? string path to wallpaper
+function M.setup(screen, wallpaper)
+    wallpaper = wallpaper or beautiful.wallpaper
+    if not wallpaper then
+        utils.notify("Wallpaper wasn't set in the theme.", { title = "Awesome Wallpaper Setter", preset = "critical" })
+        return
     end
+    if type(wallpaper) == "function" then
+        wallpaper = wallpaper(screen)
+    end
+    gears.wallpaper.maximized(wallpaper, screen, true)
 end
 
 return M
