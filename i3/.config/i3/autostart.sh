687da9445ff18d2940a20ea8e4bb6427320514bf
Arthur McLain <mclain.it@gmail.com>
2026-05-23 06:17:53 +0200

    chore: update autostart config

--- i3/.config/i3/autostart.sh
+++ i3/.config/i3/autostart.sh
@@ -22,12 +22,10 @@ run_if_exists conky --daemonize
 # Get the correct current brightness on system startup
 #~/.local/bin/brightness-control --update-actual-brightness
 
-# Sets the delay and rate for keyboard auto-repeat
+# Set the delay and rate for keyboard auto-repeat
 xset r rate 400 25
 
 # Enable multiple languages together with win-space toggle
 # Use "localectl list-x11-keymap-options | grep grp:" to determine available keymap-options
-setxkbmap -layout us,ru
-setxkbmap -option 'grp:win_space_toggle'
 # Use CAPS+"+a to compose letter ä, similarly for ö,ü,ß
-setxkbmap -option 'compose:caps'
+setxkbmap -layout us,ru -option grp:win_space_toggle,compose:caps
