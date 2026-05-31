92419f3ed35bcf6ac57be33621d070bfb507d985
Arthur McLain <mclain.it@gmail.com>
2026-06-01 01:24:40 +0200

    fix(awesome): force spotify to not launch maximized

--- awesome/.config/awesome/core/rules.lua
+++ awesome/.config/awesome/core/rules.lua
@@ -77,6 +77,17 @@ awful.rules.rules = {
     -- Add titlebars to normal clients and dialogs
     { rule_any = { type = { "normal", "dialog" } }, properties = { titlebars_enabled = env.enable_titlebars } },
 
+    -- Fix Spotify launching maximized
+    {
+        rule = { class = "Spotify" },
+        properties = {
+            floating = false,
+            maximized = false,
+            maximized_vertical = false,
+            maximized_horizontal = false,
+        },
+    },
+
     -- Set Firefox to always map on the tag named "2" on screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { screen = 1, tag = "2" } },
