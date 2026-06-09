7efa7ff4a40117106fa4b45269974528e65b4a56
Arthur McLain <mclain.it@gmail.com>
2026-06-09 07:46:38 +0200

    feat(awesome): remove deprecated themes and update my own

--- awesome/.config/awesome/theme/theme.lua
+++ awesome/.config/awesome/theme/theme.lua
@@ -1,8 +1,10 @@
------------------------------------
---       My Awesome Theme        --
---                               --
--- Inspired by the default theme --
------------------------------------
+----------------------------------------------
+--             My Awesome Theme             --
+--                                          --
+--      Inspired by the default theme       --
+--                   and                    --
+-- https://github.com/lcpz/awesome-copycats --
+----------------------------------------------
 
 -- local theme_assets = require("beautiful.theme_assets")
 local xresources = require("beautiful.xresources")
@@ -12,8 +14,12 @@ local gears = require("gears")
 local gfs = require("gears.filesystem")
 -- local themes_path = gfs.get_themes_dir() -- /usr/share/awesome/themes/
 local themes_path = gfs.get_configuration_dir() .. "theme/"
+local layouts_path = themes_path .. "layouts/light/"
+local titlebar_path = themes_path .. "titlebar/"
 
 local theme = {}
+
+theme.wallpaper = themes_path .. "wallpaper.png"
 
 theme.font = "Maple Mono NF 10"
 theme.taglist_font = "Maple Mono NF 13"
@@ -111,65 +117,48 @@ theme.notification_position = "top_right"
 -- Variables set for theming the menu:
 -- menu_[bg|fg]_[normal|focus]
 -- menu_[border_color|border_width]
-theme.menu_submenu_icon = themes_path .. "submenu.png"
+theme.menu_submenu_icon = themes_path .. "icons/submenuw.png"
 theme.menu_height = dpi(15)
 theme.menu_width = dpi(100)
 
--- You can add as many variables as
--- you wish and access them by using
--- beautiful.variable in your rc.lua
---theme.bg_widget = "#cc0000"
+-- Variables set for theming the titlebar:
+theme.titlebar_close_button_normal = titlebar_path .. "close_normal.png"
+theme.titlebar_close_button_focus = titlebar_path .. "close_focus.png"
 
--- Define the image to load
-theme.titlebar_close_button_normal = themes_path .. "titlebar/close_normal.png"
-theme.titlebar_close_button_focus = themes_path .. "titlebar/close_focus.png"
+theme.titlebar_minimize_button_normal = titlebar_path .. "minimize_normal.png"
+theme.titlebar_minimize_button_focus = titlebar_path .. "minimize_focus.png"
 
-theme.titlebar_minimize_button_normal = themes_path .. "titlebar/minimize_normal.png"
-theme.titlebar_minimize_button_focus = themes_path .. "titlebar/minimize_focus.png"
+theme.titlebar_ontop_button_normal_inactive = titlebar_path .. "ontop_normal_inactive.png"
+theme.titlebar_ontop_button_focus_inactive = titlebar_path .. "ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_active = titlebar_path .. "ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_active = titlebar_path .. "ontop_focus_active.png"
 
-theme.titlebar_ontop_button_normal_inactive = themes_path .. "titlebar/ontop_normal_inactive.png"
-theme.titlebar_ontop_button_focus_inactive = themes_path .. "titlebar/ontop_focus_inactive.png"
-theme.titlebar_ontop_button_normal_active = themes_path .. "titlebar/ontop_normal_active.png"
-theme.titlebar_ontop_button_focus_active = themes_path .. "titlebar/ontop_focus_active.png"
+theme.titlebar_sticky_button_normal_inactive = titlebar_path .. "sticky_normal_inactive.png"
+theme.titlebar_sticky_button_focus_inactive = titlebar_path .. "sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_active = titlebar_path .. "sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_active = titlebar_path .. "sticky_focus_active.png"
 
-theme.titlebar_sticky_button_normal_inactive = themes_path .. "titlebar/sticky_normal_inactive.png"
-theme.titlebar_sticky_button_focus_inactive = themes_path .. "titlebar/sticky_focus_inactive.png"
-theme.titlebar_sticky_button_normal_active = themes_path .. "titlebar/sticky_normal_active.png"
-theme.titlebar_sticky_button_focus_active = themes_path .. "titlebar/sticky_focus_active.png"
+theme.titlebar_floating_button_normal_inactive = titlebar_path .. "floating_normal_inactive.png"
+theme.titlebar_floating_button_focus_inactive = titlebar_path .. "floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_active = titlebar_path .. "floating_normal_active.png"
+theme.titlebar_floating_button_focus_active = titlebar_path .. "floating_focus_active.png"
 
-theme.titlebar_floating_button_normal_inactive = themes_path .. "titlebar/floating_normal_inactive.png"
-theme.titlebar_floating_button_focus_inactive = themes_path .. "titlebar/floating_focus_inactive.png"
-theme.titlebar_floating_button_normal_active = themes_path .. "titlebar/floating_normal_active.png"
-theme.titlebar_floating_button_focus_active = themes_path .. "titlebar/floating_focus_active.png"
+theme.titlebar_maximized_button_normal_inactive = titlebar_path .. "maximized_normal_inactive.png"
+theme.titlebar_maximized_button_focus_inactive = titlebar_path .. "maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_active = titlebar_path .. "maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_active = titlebar_path .. "maximized_focus_active.png"
 
-theme.titlebar_maximized_button_normal_inactive = themes_path .. "titlebar/maximized_normal_inactive.png"
-theme.titlebar_maximized_button_focus_inactive = themes_path .. "titlebar/maximized_focus_inactive.png"
-theme.titlebar_maximized_button_normal_active = themes_path .. "titlebar/maximized_normal_active.png"
-theme.titlebar_maximized_button_focus_active = themes_path .. "titlebar/maximized_focus_active.png"
-
-theme.wallpaper = themes_path .. "wallpaper.png"
-
--- You can use your own layout icons like this:
-theme.layout_fairh = themes_path .. "layouts/fairhw.png"
-theme.layout_fairv = themes_path .. "layouts/fairvw.png"
-theme.layout_floating = themes_path .. "layouts/floatingw.png"
-theme.layout_magnifier = themes_path .. "layouts/magnifierw.png"
-theme.layout_max = themes_path .. "layouts/maxw.png"
-theme.layout_fullscreen = themes_path .. "layouts/fullscreenw.png"
-theme.layout_tilebottom = themes_path .. "layouts/tilebottomw.png"
-theme.layout_tileleft = themes_path .. "layouts/tileleftw.png"
-theme.layout_tile = themes_path .. "layouts/tilew.png"
-theme.layout_tiletop = themes_path .. "layouts/tiletopw.png"
-theme.layout_spiral = themes_path .. "layouts/spiralw.png"
-theme.layout_dwindle = themes_path .. "layouts/dwindlew.png"
-theme.layout_cornernw = themes_path .. "layouts/cornernww.png"
-theme.layout_cornerne = themes_path .. "layouts/cornernew.png"
-theme.layout_cornersw = themes_path .. "layouts/cornersww.png"
-theme.layout_cornerse = themes_path .. "layouts/cornersew.png"
+-- Variables set for theming the layouts:
+theme.layout_tile = layouts_path .. "tile.png"
+theme.layout_tilebottom = layouts_path .. "tilebottom.png"
+theme.layout_floating = layouts_path .. "floating.png"
+theme.layout_magnifier = layouts_path .. "magnifier.png"
+theme.layout_fullscreen = layouts_path .. "fullscreen.png"
+theme.layout_dwindle = layouts_path .. "dwindle.png"
 
 -- -- Generate Awesome icon:
 -- theme.awesome_icon = theme_assets.awesome_icon(theme.menu_height, theme.bg_focus, theme.fg_focus)
-theme.awesome_icon = themes_path .. "/awesome.png"
+theme.awesome_icon = themes_path .. "icons/awesomew.png"
 
 -- Define the icon theme for application icons. If not set then the icons
 -- from /usr/share/icons and /usr/share/icons/hicolor will be used.
